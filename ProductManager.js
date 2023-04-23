65a07ca83c3e5d9f54d5d7152cc5edee48a9caf4
alpaternina <alpaternina@hotmail.com>
2023-04-23 13:34:59 -0500

    Desafio 2

--- ProductManager.js
+++ ProductManager.js
@@ -1,52 +1,115 @@
-class productManager {
-    constructor(producto) {
-      this.producto = [];
+const fs = require('fs');
+
+class ProductManager {
+  id = 1;
+  constructor(path) {
+    this.path = path;
+  }
+  
+  async addProduct(productoDato) {
+    if (!fs.existsSync(this.path)) {
+      await fs.promises.writeFile(this.path, '[]');
     }
-  
-    addProducto(title, description, price, thumbnail, code, stock) {
-      const producto = {
-        id: this.producto.length + 1,
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock,
-      };
-  
-      if (title == undefined || description == undefined || price == undefined || thumbnail == undefined || code == undefined || stock == undefined) {
-        console.log('Completar todos los campos del', title);
+
+    const { title, description, price, thumbnail, code, stock } = productoDato;
+
+    if (title == undefined || description == undefined || price == undefined || thumbnail == undefined || code == undefined || stock == undefined) {
+      return console.log('Completar todos los campos del', title);
+    } else {
+      let producto = [];
+    
+      let productoContenido = await fs.promises.readFile(this.path, 'utf-8');
+      producto = JSON.parse(productoContenido);
+
+      const productoEncontrado = producto.some((item) => item.code == code);
+      if (productoEncontrado) {
+        return console.log('El producto ya existe');
       } else {
-        const productoFound = this.producto.some((item) => item.code == code);
-        if (productoFound) {
-          console.log('El producto ya existe');
-        } else {
-          this.producto.push(producto);
+        if (producto.length > 0) {
+          this.id = producto[producto.length - 1].id + 1;
         }
-      }
-    }
-  
-    getProducto() {
-      console.log(this.producto);
-    }
-  
-    getProductoById(id) {
-      const productoFound = this.producto.find((item) => item.id == id);
-      if (productoFound) {
-        console.log('El producto encontrado es ', productoFound);
-      } else {
-        console.log('No ha sido encontrado el Producto', id);
+        const product = { id: this.id, ...productoDato };
+        producto.push(product);
+        let productString = JSON.stringify(producto, null, 2);
+        await fs.promises.writeFile(this.path, productString);
+        return console.log('Producto adicionado');
       }
     }
   }
-  
-  const productoTest = new productManager();
+
+  async getProducts() {
+    if (!fs.existsSync(this.path)) {
+      await fs.promises.writeFile(this.path, '[]');
+    }
+    let producto = [];
+
+    let productoContenido = await fs.promises.readFile(this.path, 'utf-8');
+    producto = JSON.parse(productoContenido);
+    return producto;
+  }
 
   
-  productoTest.addProducto('producto A', 'Este es un producto A', 1000, 'Sin imagen', 'AAA123', 100);
-  productoTest.addProducto('producto B', 'Este es un producto B', 1500, 'Sin imagen', 'BBB123', 150);
-  productoTest.addProducto('producto C', 'Este es un producto C', 2000, 'Sin imagen', 'CCC123', 80);
-  productoTest.addProducto('producto D', 'Este es un producto D', 2000, 'Sin imagen', 'CCC123'); // No va a mostrar esta linea porque no tiene el stock
-  productoTest.getProducto(); // Muestra Producto A, B y C
-  productoTest.getProductoById(1); // Muestra el producto A
-  productoTest.getProductoById(5); // Muestra que no ha sido encontrado el producto
+  async getProductById(id) {
+    if (!fs.existsSync(this.path)) {
+      await fs.promises.writeFile(this.path, '[]');
+    }
+    let producto = [];
+    let productoContenido = await fs.promises.readFile(this.path, 'utf-8');
+    producto = JSON.parse(productoContenido);
+
+    const productoEncontrado = producto.find((item) => item.id == id);
+    if (productoEncontrado) {
+      console.log('El producto encontrado es ', productoEncontrado);
+      return productoEncontrado;
+    } else {
+      console.log('No ha sido encontrado el Producto', id);
+    }
+  }
+}
+
+const product1 = {
+  title: 'producto prueba',
+  description: 'Este es un producto prueba',
+  price: 10,
+  thumbnail: 'Sin imagen',
+  code: 'abc123',
+  stock: 7,
+};
+
+const product2 = {
+  title: 'producto prueba 2',
+  description: 'Este es un producto prueba 2',
+  price: 10,
+  thumbnail: 'Sin imagen',
+  code: 'abc124',
+  stock: 15,
+};
+
+const productManager = new ProductManager('products.json');
+
+productManager
+  .addProduct(product1)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+productManager
+  .addProduct(product2)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  }); 
+
+productManager
+  .getProducts()
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
+ 
+productManager
+  .getProductById(0)
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
