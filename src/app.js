42b069ce1679fa05d708e7d3b03c4c06dc39468d
alpaternina <alpaternina@hotmail.com>
2023-05-10 20:42:19 -0500

    Primera Preentrega

--- src/app.js
+++ src/app.js
@@ -1,42 +1,19 @@
 import express from 'express';
-import ProductManager from './ProductManager.js';
-const container = new ProductManager('./src/products.json');
+import { productsRouter } from './routes/products.router.js';
+import { cartsRouter } from './routes/carts.router.js';
 
 const app = express ();
-
 
 app.listen(8080, () => {
   console.log('Se esta escuchando 8080');
 });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
 
-app.get('/products', async (req, res) => {
-  try {
-    const limit = req.query.limit;
-    const products = await container.getProducts();
-    if (limit) {
-      return res.json(products.slice(0, limit));
-    } else {
-      return res.json(products);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-app.get('/products/:pid', async (req, res) => {
-  try {
-    const id = req.params.pid;
-    const product = await container.getProductById(parseInt(id));
-
-    if (typeof product === 'object') {
-      return res.json(product);
-    }
-    if (typeof product === 'string') {
-      return res.json({ error: 'El producto no encontrado' });
-    }
-  } catch (error) {
-    console.log(error);
-  }
+app.get('*', (req, res) => {
+  return res.status(404).json({ status: 'error', message: 'No fue encontrado' });
 });
